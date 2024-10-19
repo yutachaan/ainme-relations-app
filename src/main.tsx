bb3f9000025448136c5f2a2f9f16fe4e8888abc7
yutachaan <yutachan010907@gmail.com>
2024-10-19 14:48:13 +0900

    テーマを適用する箇所をmain.tsxに変更

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,12 @@ import App from './App.tsx'
 import { ChakraProvider } from '@chakra-ui/react'
 import { ApolloProvider } from '@apollo/client'
 import client from './apolloClient.ts'
+import theme from './theme'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ApolloProvider client={client}>
-      <ChakraProvider>
+      <ChakraProvider theme={theme}>
         <App />
       </ChakraProvider>
     </ApolloProvider>
